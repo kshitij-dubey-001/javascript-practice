5cf1159631ea72a2f571af2e3df3bcf17aa1138a
kshitij-dubey-001 <kshitijdubey2007@gmail.com>
2026-01-20 23:54:58 +0530

    Added-new-loops

--- loops-testing.js
+++ loops-testing.js
@@ -8,3 +8,19 @@ function fizzBuzz(n) {
   }
   return result;
 }
+
+const text = "javascript loop practice";
+let count = 0;
+const vowels = "aeiou";
+
+for (let i = 0; i < text.length; i++) {
+  for (let j = 0; j < vowels.length; j++) {
+    if (text[i].toLowerCase() === vowels[j]) {
+      count++;
+      break;
+    }
+  }
+}
+
+console.log(count);
+        
