2131f9f6455a627c0fe1a70e23eaf008922fcb22
kshitij-dubey-001 <kshitijdubey2007@gmail.com>
2026-01-22 23:31:39 +0530

    Added new code to check second largest num

--- loops.js
+++ loops.js
@@ -1,3 +1,18 @@
 for(let i = 1; i <= 5; i++){
   console.log(i);
 }
+const nums = [10, 5, 20, 8, 15];
+
+let max = -Infinity;
+let secondMax = -Infinity;
+
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] > max) {
+    secondMax = max;
+    max = nums[i];
+  } else if (nums[i] > secondMax && nums[i] !== max) {
+    secondMax = nums[i];
+  }
+}
+
+console.log(secondMax);
